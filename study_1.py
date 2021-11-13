2c25d84b0517cd1b37ebb881115d2ab432a2b737
yuemengrui <1401190335@qq.com>
2021-11-13 19:04:27 +0800

    first

---
+++ study_1.py
@@ -0,0 +1,14 @@
+# *_*coding:utf-8 *_*
+import torch
+import torch.nn as nn
+from torch.autograd import variable
+import torch.utils.data as Data
+import torchvision
+import matplotlib.pyplot as plt
+
+EPOCH = 1
+BATCH_SIZE = 50
+
+
+
+train_loader = Data.
